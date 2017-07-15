f9a7882942eadca8fb0e141c07f64639a8ce7248
Nikola Kocic <nikolakocic@gmail.com>
2017-07-15 09:58:25 +0200

    Fix import-files.sh

--- import-files.sh
+++ import-files.sh
@@ -5,8 +5,9 @@ set -o errtrace
 set -o pipefail
 set -o nounset
 
+# Example: ./import-files.sh '~/Documents/torrents' '127.0.0.1' 53713 'localclient' 'pass'
 sourcedir="${1}"
 rm ./failed.txt
 for file in "${sourcedir}"/*.torrent; do
-    ./deluge_import_torrents.py "${file}" || { echo "${file}" >> ./failed.txt; }
+    ./deluge_import_torrents.py "${@:2}" "${file}" || { echo "${file}" >> ./failed.txt; }
 done
